f97babde94310585220caacf736e5efa2a58259e
Yashwanth B M <yashwanthbm36@gmail.com>
2024-01-22 00:53:03 +0530

    made this production ready

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import server from "./index.js";
-server.listen(3400, (err) => {
+import 'dotenv/config'
+server.listen(process.env.PORT, (err) => {
     if (err) console.log("ERROR :: server.js :: Server listen Error")
 
     console.log("Server is live on PORT --> 3400");
